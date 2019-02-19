ee6e37224301b8985f9f36e8d9346d24f4fae347
Administrator <496057338@qq.com>
2019-02-19 23:09:56 +0800

    Initial commit

---
+++ src/main/java/com/saltfish/spiderbasewebmagic/SpiderBaseWebmagicApplication.java
@@ -0,0 +1,13 @@
+package com.saltfish.spiderbasewebmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderBaseWebmagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderBaseWebmagicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saltfish/spiderbasewebmagic/SpiderBaseWebmagicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saltfish.spiderbasewebmagic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpiderBaseWebmagicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
